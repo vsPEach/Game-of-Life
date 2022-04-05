ab86c14d04462a2bbe5aa3ef8aeddea545e82285
vsPEach <pers1kwtf@gmail.com>
2022-04-05 11:03:51 +0300

    Update count_alive_neighbours function

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ void set_alive(char** field, int x, int y)
     field[x][y] = '*';
 }
 
-char** init_field(int& x, int& y)
+char** init_field(int& rows, int& columns)
 {
     std::ifstream f("setup.txt");
-    f >> x >> y;
-    char** field = new char *[x];
-    for (int i = 0; i < x; i++)
+    f >> rows >> columns;
+    char** field = new char *[rows];
+    for (int i = 0; i < rows; i++)
     {
-        field[i] = new char [y];
+        field[i] = new char [columns];
     }
-    for (int i = 0; i < x; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < y; j++)
+        for (int j = 0; j < columns; j++)
         {
             field[i][j] = '-';
         }
@@ -26,15 +26,15 @@ char** init_field(int& x, int& y)
 
     while (!f.eof())
     {
-        int rows, columns;
-        f >> rows >> columns;
-        set_alive(field, rows, columns);
+        int x, y;
+        f >> x >> y;
+        set_alive(field, x, y);
     }
 
     return  field;
 }
 
-void print(char **field, int x, int y)
+void print(char** field, int x, int y)
 {
     for (int i = 0; i < x; i++)
     {
@@ -46,9 +46,81 @@ void print(char **field, int x, int y)
     }
 }
 
+void print(char** field, int x, int y, bool f)
+{
+    std::cout << field[x][y] << std::endl;
+}
+
+int count_alive_neighbours(char **field, int x, int y, int rows, int columns)
+{
+    if (x == rows - 1 and y == 0)
+    {
+        int neighbours = 0;
+        if (field[x - 1][y] == '*') neighbours++;
+        if (field[x - 1][y + 1] == '*') neighbours++;
+
+    }
+
+    if (x == rows - 1 and y == columns - 1)
+    {
+        int neighbours = 0;
+        if (field[x - 1][y - 1] == '*') neighbours++;
+
+        if (field[x - 1][y] == '*') neighbours++;
+
+        if (field[x][y - 1] == '*') neighbours++;
+        return neighbours;
+    }
+
+    if (x == 0 and y == 0)
+    {
+        int neighbours = 0;
+        if (field[x][y + 1] == '*') neighbours++;
+
+        if (field[x + 1][y] == '*') neighbours++;
+
+        if (field[x + 1][y + 1] == '*') neighbours++;
+
+        return neighbours;
+    }
+
+    if (x == 0 and y > 0)
+    {
+        int neighbours = 0;
+        if (field[x][y + 1] == '*') neighbours++;
+        if (field[x][y - 1] == '*') neighbours++;
+
+        if (field[x + 1][y] == '*') neighbours++;
+        if (field[x + 1][y + 1] == '*') neighbours++;
+        if (field[x + 1][y - 1] == '*') neighbours++;
+
+        return neighbours;
+    }
+
+    if ( rows > x > 0 and columns > y > 0)
+    {
+        int neighbours = 0;
+
+        if (field[x + 1][y + 1] == '*') neighbours++;
+        if (field[x + 1][y] == '*') neighbours++;
+        if (field[x + 1][y - 1] == '*') neighbours++;
+
+        if (field[x][y + 1] == '*') neighbours++;
+        if (field[x][y - 1] == '*') neighbours++;
+
+        if (field[x - 1][y + 1] == '*') neighbours++;
+        if (field[x - 1][y] == '*') neighbours++;
+        if (field[x - 1][y - 1] == '*') neighbours++;
+        return neighbours;
+    }
+
+}
+
+
 int main(int argc, char *argv[])
 {
-    int x, y;
-    char **field = init_field(x, y);
-    print(field, x, y);
+    int rows, columns;
+    char **field = init_field(rows, columns);
+    print(field, rows, columns);
+    std::cout << count_alive_neighbours(field, 3, 4, rows, columns);
 }
