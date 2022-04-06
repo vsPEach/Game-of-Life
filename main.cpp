2e015fd4dbfde9d45723a2f42f98a954aee96f89
vsPEach <pers1kwtf@gmail.com>
2022-04-06 20:17:36 +0300

    create generation function and update counting neighbours function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "Simplify"
 #include <iostream>
 #include <thread>
 #include <fstream>
@@ -51,85 +53,73 @@ void print(char** field, int x, int y, bool f)
     std::cout << field[x][y] << std::endl;
 }
 
-int count_alive_neighbours(char **field, int x, int y, int rows, int columns)
-{
-    if (x == 0 and y == 0)
+void remove_field(char** field, int rows, int columns);
+
+int count_alive_neighbours(char** field, int x, int y, int rows, int columns) {
+    int neighbours = 0;
+    for (int i = -1; i <= 1; i++)
     {
-        int neighbours = 0;
-        if (field[x][y + 1] == '*') neighbours++;
+        for (int j = -1; j <= 1; j++)
+        {
+            if (i == 0 and j == 0) continue; //without counting itself
 
-        if (field[x + 1][y] == '*') neighbours++;
+            if (x == 0 and y == 0)
+                if (i == - 1 and j == -1) continue; //for top left corner
 
-        if (field[x + 1][y + 1] == '*') neighbours++;
+            if (x == 0 and y == columns - 1)
+                if ((i == -1) or (i == 0 and j == -1) or (i == 1 and j == 1)) continue; //for top right corner
 
-        return neighbours;
+            if (x == rows - 1 and y == 0)
+                if ((i == 1) or (i == -1 and j == -1) or (i == 0 and j == -1)) continue; //for bottom left corner
+
+            if (x == rows - 1 and y == columns - 1)
+                if ((i == 1) or (i == 0 and j == 1) or (i == -1 and j == 1)) continue; // for bottom right corner
+
+            if (0 < x < rows - 1 and y == 0)
+                if (j == -1) continue;
+
+            if (0 < y < columns - 1 and x == 0)
+                if (i == -1) continue;
+
+            if (y == columns - 1 and  0 < x < rows - 1)
+                if (j == 1) continue;
+
+            if (x == rows - 1 and 0 < y < columns - 1)
+                if (i == 1) continue;
+
+            if (field[x + i][y + j] == '*') neighbours++;
+        }
     }
 
-    if (x == rows - 1 and y == 0)
-    {
-        int neighbours = 0;
-        if (field[x - 1][y] == '*') neighbours++;
-        if (field[x - 1][y + 1] == '*') neighbours++;
+    return neighbours;
+}
 
-    }
+void check_changes(char** field, int rows, int columns, bool& life);
 
-    if (x == rows - 1 and y == columns - 1)
-    {
-        int neighbours = 0;
-        if (field[x - 1][y - 1] == '*') neighbours++;
+char** next_generation(char** current_field, int rows, int columns) {
 
-        if (field[x - 1][y] == '*') neighbours++;
+    char** next_field = current_field;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++) {
+            if (current_field[i][j] == '-')
+                if (count_alive_neighbours(current_field, i, j, rows, columns) == 3)
+                    set_alive(next_field, i, j);
+            if (current_field[i][j] == '*')
+                if (count_alive_neighbours(current_field, i, j, rows, columns) > 3 or count_alive_neighbours(current_field, i, j, rows, columns) < 2)
+                    next_field[i][j] = '-';
 
-        if (field[x][y - 1] == '*') neighbours++;
-        return neighbours;
-    }
+        }
 
-    if (x == 0 and y == columns - 1)
-    {
-        int neighbours = 0;
-        if (field[x - 1][y] == '*') neighbours++;
-        if (field[x - 1][y - 1] == '*') neighbours++;
-        if (field[x][y - 1] == '*') neighbours++;
-        return neighbours;
-    }
-
-    if (x == 0 and y > 0)
-    {
-        int neighbours = 0;
-        if (field[x][y + 1] == '*') neighbours++;
-        if (field[x][y - 1] == '*') neighbours++;
-
-        if (field[x + 1][y] == '*') neighbours++;
-        if (field[x + 1][y + 1] == '*') neighbours++;
-        if (field[x + 1][y - 1] == '*') neighbours++;
-
-        return neighbours;
-    }
-
-    if ( rows > x > 0 and columns > y > 0)
-    {
-        int neighbours = 0;
-
-        if (field[x + 1][y + 1] == '*') neighbours++;
-        if (field[x + 1][y] == '*') neighbours++;
-        if (field[x + 1][y - 1] == '*') neighbours++;
-
-        if (field[x][y + 1] == '*') neighbours++;
-        if (field[x][y - 1] == '*') neighbours++;
-
-        if (field[x - 1][y + 1] == '*') neighbours++;
-        if (field[x - 1][y] == '*') neighbours++;
-        if (field[x - 1][y - 1] == '*') neighbours++;
-        return neighbours;
-    }
-
+    return next_field;
 }
 
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     int rows, columns;
     char **field = init_field(rows, columns);
     print(field, rows, columns);
-    std::cout << count_alive_neighbours(field, 0, 0, rows, columns);
+    std::cout << '\n';
+    char **next_field = next_generation(field, rows, columns);
+    print(next_field, rows, columns);
 }
